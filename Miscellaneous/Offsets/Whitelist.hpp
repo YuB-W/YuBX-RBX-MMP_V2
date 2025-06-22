3c3e1bfd7f15268824317cc5129d6d871ef92fea
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-06-22 12:10:17 +0300

    Update Whitelist.hpp

--- Miscellaneous/Offsets/Whitelist.hpp
+++ Miscellaneous/Offsets/Whitelist.hpp
@@ -5,7 +5,7 @@
 namespace Whitelist_Offsets
 {
 	// these offsets change every update
-	static const uintptr_t set_insert = 0xD0D0F0;
-	static const uintptr_t Bitmap = 0x2A6938;
-	static const uintptr_t whitelist_page = 0x2A5A70;
+	static const uintptr_t set_insert = 0xDA4E70;
+	static const uintptr_t Bitmap = 0x298668;
+	static const uintptr_t whitelist_page = 0x297EC8;
 }
